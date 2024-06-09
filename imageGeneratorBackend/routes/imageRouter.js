123ff7129caf02c9ddec4e4aae6932b63674b4f2
Govind Gupta <gvgupta789@gmail.com>
2024-06-09 20:27:43 +0530

    commit

--- imageGeneratorBackend/routes/imageRouter.js
+++ imageGeneratorBackend/routes/imageRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ImageRouter = express.Router();
 const {generateImage} = require('../controllers/imageControllers.js');
 
-ImageRouter.route('/generate')
+ImageRouter.route('/')
     .post(generateImage);
 
 module.exports = ImageRouter;
