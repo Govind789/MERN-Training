9595510bc2781c5a4bd7a4b663eee77316102d32
Govind Gupta <gvgupta789@gmail.com>
2024-06-10 19:28:28 +0530

    commit

--- imageGeneratorBackend/routes/authRoutes.js
+++ imageGeneratorBackend/routes/authRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const { signup,login} = require('../controllers/authControllers.js');
 const authRouter = express.Router();
 
-authRouter.route('/api/v1/auth/signup')
+authRouter.route('/signup')
     .post(signup)
 
 
-authRouter.route('/api/v1/auth/login')
+authRouter.route('/login')
     .post(login)
 
 module.exports = authRouter;
--- imageGeneratorBackend/routes/imageRouter.js
+++ imageGeneratorBackend/routes/imageRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ImageRouter = express.Router();
 const {generateImage} = require('../controllers/imageControllers.js');
 
-ImageRouter.route('/api/v1/images')
+ImageRouter.route('/')
     .post(generateImage);
 
 module.exports = ImageRouter;
