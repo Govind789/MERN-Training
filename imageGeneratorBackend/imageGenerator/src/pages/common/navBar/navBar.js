f54b8a3f04a33285c46635e1e338509af861e020
Govind Gupta <gvgupta789@gmail.com>
2024-06-10 14:58:22 +0530

    commit

--- imageGeneratorBackend/imageGenerator/src/pages/common/navBar/navBar.js
+++ imageGeneratorBackend/imageGenerator/src/pages/common/navBar/navBar.js
@@ -11,17 +11,16 @@ const NavBar = (props)=>{
     const customColor = (e)=>{
         return { color: page === e?'red':cssName};
     }
-    const pth = 'https://mern-training-0ufm.onrender.com/api/v1/images'
     return(
         <div className="header-parent-container">
             <div className="left">
-                <Link to="https://mern-training-0ufm.onrender.com/api/v1/images/home" style={customColor('HomePage')}>Home</Link>
-                <Link to="https://mern-training-0ufm.onrender.com/api/v1/images/imageGenerator" style={customColor('imageGenerator')}>Image Generator</Link>
-                <Link to="https://mern-training-0ufm.onrender.com/api/v1/images/history" style={customColor('history')}>History</Link>
-                <Link to="https://mern-training-0ufm.onrender.com/api/v1/images/contactUs" style={customColor('contactUs')}>Contact Us</Link>
-                <Link to="https://mern-training-0ufm.onrender.com/api/v1/images/help" style={customColor('help')}>Help</Link>
-                <Link to="https://mern-training-0ufm.onrender.com/api/v1/images/signup" style={customColor('signup')}>Signup</Link>
-                <Link to="https://mern-training-0ufm.onrender.com/api/v1/images/login" style={customColor('login')}>Login</Link>
+                <Link to="/home" style={customColor('HomePage')}>Home</Link>
+                <Link to="/imageGenerator" style={customColor('imageGenerator')}>Image Generator</Link>
+                <Link to="/history" style={customColor('history')}>History</Link>
+                <Link to="/contactUs" style={customColor('contactUs')}>Contact Us</Link>
+                <Link to="/help" style={customColor('help')}>Help</Link>
+                <Link to="/signup" style={customColor('signup')}>Signup</Link>
+                <Link to="/login" style={customColor('login')}>Login</Link>
             </div>
             <div className="right" style={{padding: '4px', color: 'brown'}}>
             </div>
