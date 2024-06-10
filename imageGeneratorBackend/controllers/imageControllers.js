aa48cd57a0b4d011db37845d64f827ee50b01409
Govind Gupta <gvgupta789@gmail.com>
2024-06-10 22:22:53 +0530

    commit

--- imageGeneratorBackend/controllers/imageControllers.js
+++ imageGeneratorBackend/controllers/imageControllers.js
@@ -9,10 +9,10 @@ const generateImage = async (req, res) => {
         const res = await fetch("https://api.hotpot.ai/art-maker-sdte-zmjbcrr", {
             "headers": {
               "accept": "*/*",
-              "accept-language": "en-GB,en;q=0.5",
-              "api-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MTc4NDc2NzAsImV4cCI6MTcxNzkzNDA3MH0.mpYduY35e_6s7-pzfgY6FLl7UdovK_qtgd1c2yujzIQ",
+              "accept-language": "en-GB,en;q=0.7",
+              "api-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MTgwMzc5NDksImV4cCI6MTcxODEyNDM0OX0.-ErMGa-LNzBzcPouyfCYQpBQq9N0vwoLzkojty1XDe4",
               "authorization": "hotpot-t2mJbCr8292aQzp8CnEPaK",
-              "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryEBfFOIAV5cMGEmsu",
+              "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryqMrPJoML6HWyc4FD",
               "sec-ch-ua": "\"Brave\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
               "sec-ch-ua-mobile": "?0",
               "sec-ch-ua-platform": "\"Windows\"",
@@ -23,7 +23,7 @@ const generateImage = async (req, res) => {
             },
             "referrer": "https://hotpot.ai/",
             "referrerPolicy": "strict-origin-when-cross-origin",
-            "body": `------WebKitFormBoundaryEBfFOIAV5cMGEmsu\r\nContent-Disposition: form-data; name=\"seedValue\"\r\n\r\nnull\r\n------WebKitFormBoundaryEBfFOIAV5cMGEmsu\r\nContent-Disposition: form-data; name=\"inputText\"\r\n\r\nc${searchText}\r\n------WebKitFormBoundaryEBfFOIAV5cMGEmsu\r\nContent-Disposition: form-data; name=\"width\"\r\n\r\n512\r\n------WebKitFormBoundaryEBfFOIAV5cMGEmsu\r\nContent-Disposition: form-data; name=\"height\"\r\n\r\n512\r\n------WebKitFormBoundaryEBfFOIAV5cMGEmsu\r\nContent-Disposition: form-data; name=\"styleId\"\r\n\r\n49\r\n------WebKitFormBoundaryEBfFOIAV5cMGEmsu\r\nContent-Disposition: form-data; name=\"styleLabel\"\r\n\r\nPhoto General 1\r\n------WebKitFormBoundaryEBfFOIAV5cMGEmsu\r\nContent-Disposition: form-data; name=\"isPrivate\"\r\n\r\nfalse\r\n------WebKitFormBoundaryEBfFOIAV5cMGEmsu\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n0\r\n------WebKitFormBoundaryEBfFOIAV5cMGEmsu\r\nContent-Disposition: form-data; name=\"requestId\"\r\n\r\n8-lZClFPK2EgfWQwO\r\n------WebKitFormBoundaryEBfFOIAV5cMGEmsu\r\nContent-Disposition: form-data; name=\"resultUrl\"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/8-lZClFPK2EgfWQwO.png\r\n------WebKitFormBoundaryEBfFOIAV5cMGEmsu--\r\n`,
+            "body": `------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"seedValue\"\r\n\r\nnull\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"inputText\"\r\n\r\n${searchText}\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"width\"\r\n\r\n512\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"height\"\r\n\r\n512\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"styleId\"\r\n\r\n49\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"styleLabel\"\r\n\r\nPhoto General 1\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"isPrivate\"\r\n\r\nfalse\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n0\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"requestId\"\r\n\r\n8-DY2h49K1WHn1Tk4\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"resultUrl\"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/8-DY2h49K1WHn1Tk4.png\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD--\r\n`,
             "method": "POST",
             "mode": "cors",
             "credentials": "include"
