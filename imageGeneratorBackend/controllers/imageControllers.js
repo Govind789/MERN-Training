ee17aa3e67fdd6dd975dc00a259b64310988a079
Govind Gupta <gvgupta789@gmail.com>
2024-06-12 02:44:18 +0530

    commit

--- imageGeneratorBackend/controllers/imageControllers.js
+++ imageGeneratorBackend/controllers/imageControllers.js
@@ -6,29 +6,26 @@ const generateImage = async (req, res) => {
 
     let imageUrl = "";
     try{
-        const res = await fetch("https://api.hotpot.ai/art-maker-sdte-zmjbcrr", {
+        const res = await fetch(`https://unsplash.com/nautocomplete/${searchText}`, {
             "headers": {
               "accept": "*/*",
-              "accept-language": "en-GB,en;q=0.7",
-              "api-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MTgwMzc5NDksImV4cCI6MTcxODEyNDM0OX0.-ErMGa-LNzBzcPouyfCYQpBQq9N0vwoLzkojty1XDe4",
-              "authorization": "hotpot-t2mJbCr8292aQzp8CnEPaK",
-              "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryqMrPJoML6HWyc4FD",
-              "sec-ch-ua": "\"Brave\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
+              "accept-language": "en-US",
+              "sec-ch-ua": "\"Not A(Brand\";v=\"99\", \"Google Chrome\";v=\"121\", \"Chromium\";v=\"121\"",
               "sec-ch-ua-mobile": "?0",
               "sec-ch-ua-platform": "\"Windows\"",
               "sec-fetch-dest": "empty",
               "sec-fetch-mode": "cors",
-              "sec-fetch-site": "same-site",
-              "sec-gpc": "1"
+              "sec-fetch-site": "same-origin"
             },
-            "referrer": "https://hotpot.ai/",
-            "referrerPolicy": "strict-origin-when-cross-origin",
-            "body": `------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"seedValue\"\r\n\r\nnull\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"inputText\"\r\n\r\n${searchText}\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"width\"\r\n\r\n512\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"height\"\r\n\r\n512\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"styleId\"\r\n\r\n49\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"styleLabel\"\r\n\r\nPhoto General 1\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"isPrivate\"\r\n\r\nfalse\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n0\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"requestId\"\r\n\r\n8-DY2h49K1WHn1Tk4\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD\r\nContent-Disposition: form-data; name=\"resultUrl\"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/8-DY2h49K1WHn1Tk4.png\r\n------WebKitFormBoundaryqMrPJoML6HWyc4FD--\r\n`,
-            "method": "POST",
+            "referrer": "https://unsplash.com/",
+            "referrerPolicy": "origin-when-cross-origin",
+            "body": null,
+            "method": "GET",
             "mode": "cors",
             "credentials": "include"
           });
         imageUrl  = await res.json();
+        console.log(imageUrl);
 
         await imageModel.create({
             searchText: searchText,
